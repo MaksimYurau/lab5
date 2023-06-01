f08f889c5e2f8b593273cb64b076152899b0e168
MaksimYurau <2004softwaredeveloper@gmail.com>
2023-06-01 19:35:17 +0300

    Updated lab5.cpp. Added Hexagonal_Prisma class.

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -1,6 +1,49 @@
 ﻿#include <iostream>
+#include <cmath>
+
+class Hexagonal_Prisma {
+private:
+	double R;
+	double H;
+public:
+	Hexagonal_Prisma(double R, double H) {
+		this->R = R;
+		this->H = H;
+	}
+
+	double get_base_area() {
+		double a = 3.0 * sqrt(3.0 / 2.0) * R;
+		return a;
+	}
+
+	double get_lateral_area() {
+		return 6 * R * H;
+	}
+
+	double get_total_area() {
+		return (2 * (3.0 * sqrt(3.0 / 2.0) * R)) + (6 * R * H);
+	}
+};
 
 int main()
 {
-    std::cout << "Hello world!";
+	setlocale(0, "Ru");
+
+	double R, H;
+	std::cout << "Введите радиус прямой призмы: ";
+	std::cin >> R;
+	std::cout << "Введите высоту прямой призмы: ";
+	std::cin >> H;
+
+	Hexagonal_Prisma prism(R, H);
+
+	double base_area = prism.get_base_area();
+	double lateral_area = prism.get_lateral_area();
+	double total_area = prism.get_total_area();
+
+	std::cout << "Площадь основания прямой призмы: " << base_area << std::endl;
+	std::cout << "Площадь боковой поверхности прямой призмы: " << lateral_area << std::endl;
+	std::cout << "Площадь полной поверхности прямой призмы: " << total_area << std::endl;
+
+	return 0;
 }
